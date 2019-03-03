4d9d7ef00656f6053a17257d07076ebb4bf61810
srn467 <467tn.mi@gmail.com>
2019-03-03 17:55:08 +0900

    upgrade

---
+++ js/index_colum.js
@@ -0,0 +1,27 @@
+//slider
+$(document).ready(function(){
+    $('.bxslider').bxSlider({
+        auto: true,
+    });
+});
+//fadein
+$(function() {
+    $('.cn').on('inview', function(event, isInView, visiblePartX, visiblePartY) {
+        if(isInView){
+            $(this).stop().addClass('cn_show');
+        }
+        else{
+            $(this).stop().removeClass('cn_show');
+        }
+    });
+});
+$(function() {
+    $('.example').on('inview', function(event, isInView, visiblePartX, visiblePartY) {
+        if(isInView){
+             $(this).stop().addClass('fadein_example');
+        }
+        else{
+            $(this).stop().removeClass('fadein_example');
+        }
+    });
+});
